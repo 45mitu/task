1614168159e8014514520aa3b484122441f74ce6
nryuji514 <e1922048@oit.ac.jp>
2024-12-27 16:33:36 +0900

    ファイル修正

--- TaskDisp.py
+++ TaskDisp.py
@@ -2,6 +2,7 @@ import customtkinter as ctk
 import datetime
 import pytz
 import os
+
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
